2634cfa47fbd8b2129985fa4dc71acfb2d798d0f
Csaba Gyuris <gycsaba@vasvari.hu>
2022-01-12 12:29:19 +0100

    Az átlag feladat

---
+++ atlag.py
@@ -0,0 +1,28 @@
+# osszeg-gyak.py
+
+# 1. feladat
+# Határozza meg a számok átlagát
+def feladat01_atlag() -> int:
+    szamok = [1.2, 2.3, 3.2, 4.1, 2.4, 2.3, 1.9, 0.1]
+    return 0
+
+# 2. feladat
+# Határozza meg a diák jegyeinek átlagát két tizedes jegy pontossággal
+def feladat02_osszeg() -> int:
+    szamok = [5, 3, 2, 4, 4, 3, 5, 5]
+    return 0
+
+# 3. feladat
+# Határozza meg a páratlan számok átlagát három tizetedes jegy pontossággal
+def feladat03_osszeg() -> int:
+    szamok = [2, 3, 4, 7, 1, 8, 6, 1, 2, 3, 7, 2, 2, 9, 6, 6]
+    return 0
+
+# 4. feladat
+# Határozza meg az első számsorozat páros számainak átlagát
+# Határozza meg az násodik számsorozat páratlan számainak átlagát
+# A függvény adja vissza a nagyobb átlagot
+def feladat04_osszeg() -> int:
+    szamok1 = [2, 3, 4, 6, 1, 8, 6, 1]
+    szamok2 = [3, 2, 4, 1, 5, 4, 7, 0]
+    return 0
